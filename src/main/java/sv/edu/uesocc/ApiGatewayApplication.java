a579d3b93121d3d075b3f5506c4f8357a61ea5fc
Melbin <melbin.cruz40@gmail.com>
2019-10-21 23:11:38 -0600

    First Commit

---
+++ src/main/java/sv/edu/uesocc/ApiGatewayApplication.java
@@ -0,0 +1,40 @@
+package sv.edu.uesocc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import sv.edu.uesocc.filters.ErrorFilter;
+import sv.edu.uesocc.filters.PostFilter;
+import sv.edu.uesocc.filters.PreFilter;
+import sv.edu.uesocc.filters.RouteFilter;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+	
+    @Bean
+    public PreFilter preFilter() {
+        return new PreFilter();
+    }
+    @Bean
+    public PostFilter postFilter() {
+        return new PostFilter();
+    }
+    @Bean
+    public ErrorFilter errorFilter() {
+        return new ErrorFilter();
+    }
+    @Bean
+    public RouteFilter routeFilter() {
+        return new RouteFilter();
+    }
+    
+}
