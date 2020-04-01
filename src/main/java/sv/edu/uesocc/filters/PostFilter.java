78c8ce7e8b09256e134c1ee62aa8b699be338a90
Melbin <melbin.cruz40@gmail.com>
2020-04-01 15:42:13 -0600

    Refresh Token as Cookie

--- src/main/java/sv/edu/uesocc/filters/PostFilter.java
+++ src/main/java/sv/edu/uesocc/filters/PostFilter.java
@@ -1,6 +1,18 @@
 package sv.edu.uesocc.filters;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.Map;
+import javax.servlet.http.Cookie;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+import io.micrometer.core.instrument.util.IOUtils;
 
 public class PostFilter extends ZuulFilter {
  
@@ -18,11 +30,42 @@ public class PostFilter extends ZuulFilter {
   public boolean shouldFilter() {
     return true;
   }
+  
+  private ObjectMapper mapper = new ObjectMapper();
  
+//  @Override
+//  public Object run() {
+//   System.out.println("Inside Response Filter");
+// 
+//    return null;
+//  }
+  
+
   @Override
   public Object run() {
-   System.out.println("Inside Response Filter");
- 
-    return null;
+      RequestContext ctx = RequestContext.getCurrentContext();
+      try {
+          InputStream is = ctx.getResponseDataStream();
+          String responseBody = IOUtils.toString(is, Charset.forName("UTF8"));
+          if (responseBody.contains("refresh_token")) {
+              Map<String, Object> responseMap = mapper.readValue(
+                responseBody, new TypeReference<Map<String, Object>>() {});
+              String refreshToken = responseMap.get("refresh_token").toString();
+              responseMap.remove("refresh_token");
+              responseBody = mapper.writeValueAsString(responseMap);
+
+              Cookie cookie = new Cookie("refreshToken", refreshToken);
+              cookie.setHttpOnly(true);
+              cookie.setSecure(true);
+              cookie.setPath(ctx.getRequest().getContextPath() + "/oauth/token");
+              cookie.setMaxAge(2592000); // 30 days
+              ctx.getResponse().addCookie(cookie);
+          }
+          ctx.setResponseBody(responseBody);
+      } catch (IOException e) {
+          System.out.println("Error occured in zuul post filter"+ e);
+      }
+      return null;
   }
+  
 }
--- src/main/java/sv/edu/uesocc/filters/PreFilter.java
+++ src/main/java/sv/edu/uesocc/filters/PreFilter.java
@@ -1,32 +1,85 @@
 package sv.edu.uesocc.filters;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
- 
+
+import sv.edu.uesocc.utils.CustomHttpServletRequest;
+
 public class PreFilter extends ZuulFilter {
- 
-  @Override
-  public String filterType() {
-    return "pre";
-  }
- 
-  @Override
-  public int filterOrder() {
-    return 1;
-  }
- 
-  @Override
-  public boolean shouldFilter() {
-    return true;
-  }
- 
-  @Override
-  public Object run() {
-    RequestContext ctx = RequestContext.getCurrentContext();
-    HttpServletRequest request = ctx.getRequest();
- 
-    System.out.println("Request Method : " + request.getMethod() + " Request URL : " + request.getRequestURL().toString());
-    return null;
-  }
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    
+    @Autowired
+    private Environment env;
+
+    @Override
+    public Object run() {
+        final RequestContext ctx = RequestContext.getCurrentContext();
+        logger.info("in zuul filter " + ctx.getRequest().getRequestURI());
+        
+        byte[] encoded;
+        try {
+        	encoded = Base64.getEncoder().encode((env.getProperty("security.oauth2.client.client-id")+":"+env.getProperty("security.oauth2.client.client-secret")).getBytes("UTF-8"));
+            ctx.addZuulRequestHeader("Authorization", "Basic " + new String(encoded));
+            logger.info("pre filter");
+            logger.info(ctx.getRequest().getHeader("Authorization"));
+
+            final HttpServletRequest req = ctx.getRequest();
+
+            final String refreshToken = extractRefreshToken(req);
+            if (refreshToken != null) {
+                final Map<String, String[]> param = new HashMap<String, String[]>();
+                param.put("refresh_token", new String[] { refreshToken });
+                param.put("grant_type", new String[] { "refresh_token" });
+
+                ctx.setRequest(new CustomHttpServletRequest(req, param));
+            }
+
+        } catch (final UnsupportedEncodingException e) {
+            logger.error("Error occured in pre filter", e);
+        }
+
+        //
+
+        return null;
+    }
+
+    private String extractRefreshToken(HttpServletRequest req) {
+        final Cookie[] cookies = req.getCookies();
+        if (cookies != null) {
+            for (int i = 0; i < cookies.length; i++) {
+                if (cookies[i].getName().equalsIgnoreCase("refreshToken")) {
+                    return cookies[i].getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public int filterOrder() {
+        return -2;
+    }
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
 }
